b6f47282cbbbf2140e6bb14d27547a076078cce3
Heloise Taillet <heloise96@hotmail.com>
2019-03-25 21:17:35 -0400

    scoreboard

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -26,18 +26,18 @@ const ScoreBoard = props => {
     <div className="ui statistics" id="stats">
       <div className="statistic">
         <div className="value">
-        {props.playerScore}
-        </div>
-        <div className="label">
-        Player Score
-        </div>
-      </div>
-      <div className="statistic">
-        <div className="value">
           {props.computerScore}
         </div>
         <div className="label">
           Computer Score
+        </div>
+      </div>
+      <div className="statistic">
+        <div className="value">
+        {props.playerScore}
+        </div>
+        <div className="label">
+        Player Score
         </div>
       </div>
     </div>
